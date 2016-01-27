c829270a9a68af401a2b43aa842e9397aac8a29c
Christian Emmer <emmercm@gmail.com>
2016-01-27 16:53:53 -0500

    Added input video file support. Renamed existing input and webcam CLI arguments.

--- SharkCV.py
+++ SharkCV.py
@@ -21,15 +21,19 @@ logging.debug('Starting %s', os.path.splitext(__file__)[0])
 
 # Parse arguments
 parser = argparse.ArgumentParser(prog=__file__)
-parser.add_argument('-i', metavar='file', dest='input_image', help='input image')
-parser.add_argument('-wi', metavar='N', dest='webcam_index', help='webcam index (default: -1)', type=int, default=-1)
-parser.add_argument('-ww', metavar='N', dest='webcam_width', help='webcam width', type=int)
-parser.add_argument('-wh', metavar='N', dest='webcam_height', help='webcam height', type=int)
-parser.add_argument('-wf', metavar='N', dest='webcam_fps', help='webcam fps (default: 30)', type=int, default=30)
+parser.add_argument('-ii', metavar='file', dest='input_image', help='input image')
+parser.add_argument('-iv', metavar='N', dest='input_video', help='input video/webcam (default: -1)', default=-1)
+parser.add_argument('-vw', metavar='N', dest='video_width', help='video width', type=int)
+parser.add_argument('-vh', metavar='N', dest='video_height', help='video height', type=int)
+parser.add_argument('-vf', metavar='N', dest='video_fps', help='video fps (webcam default: 30.0)', type=float, default=30.0)
 parser.add_argument('-oi', metavar='file', dest='output_image', help='output image')
 parser.add_argument('-ov', metavar='file', dest='output_video', help='output video')
 parser.add_argument('module', nargs='?', help='python module file')
 args = parser.parse_args()
+
+# Massage arguments
+if str(args.input_video).lstrip('-').isdigit():
+	args.input_video = int(args.input_video)
 
 
 # Dynamic import a Python algorithm file
@@ -82,20 +86,26 @@ else:
 
 
 # Main image loop
-logging.debug('Opening webcam')
+logging.debug('Opening video')
 while True:
-	# Open webcam and set options
+	# Open input video and set options
 	cap = None
 	if args.input_image is None:
-		cap = cv2.VideoCapture(args.webcam_index)
+		cap = cv2.VideoCapture(args.input_video)
 		if cap.isOpened():
-			if not args.webcam_width is None:
-				cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, args.webcam_width)
-			if not args.webcam_height is None:
-				cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, args.webcam_height)
-			if not args.webcam_fps is None:
-				cap.set(cv2.cv.CV_CAP_PROP_FPS, args.webcam_fps)
-			logging.info('Opened webcam @ %.fx%.f', cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH), cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+			# Get FPS from video file
+			if not type(args.input_video) is int:
+				args.video_fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+
+			# Set video options
+			if not args.video_width is None:
+				cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, args.video_width)
+			if not args.video_height is None:
+				cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, args.video_height)
+			if not args.video_fps is None:
+				cap.set(cv2.cv.CV_CAP_PROP_FPS, args.video_fps)
+
+			logging.info('Opened video: %.fx%.f @ %.1f FPS', cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH), cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT), args.video_fps)
 
 	# Open output video file
 	out = None
@@ -103,7 +113,7 @@ while True:
 		if not cap is None:
 			logging.debug('Opening output video: %s', args.output_video)
 			fourcc = cv2.cv.CV_FOURCC(*'DIVX')
-			out = cv2.VideoWriter(args.output_video, fourcc, args.webcam_fps, (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))))
+			out = cv2.VideoWriter(args.output_video, fourcc, args.video_fps, (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))))
 
 	# Set up FPS list and iterator
 	times = [0] * 25
@@ -115,13 +125,6 @@ while True:
 		# Get a frame to process
 		frame = None
 
-		# Read webcam frame
-		if args.input_image is None and not cap is None and cap.isOpened():
-			ret, frame = cap.read()
-			if not ret:
-				logging.warning('Failed to read webcam frame')
-				break
-
 		# Read input image
 		if not args.input_image is None:
 			if not os.path.exists(args.input_image):
@@ -129,6 +132,14 @@ while True:
 				sys.exit(1)
 			logging.info('Reading image: %s', args.input_image)
 			frame = cv2.imread(args.input_image, cv2.IMREAD_COLOR)
+
+		# Read input video
+		if not cap is None and cap.isOpened():
+			ret, frame = cap.read()
+			if not ret:
+				if type(args.input_video) is int:
+					logging.warning('Failed to read webcam frame')
+				break
 
 		if frame is None:
 			logging.error('Failed to get a frame')
@@ -172,12 +183,12 @@ while True:
 	if not out is None:
 		out.release()
 
-	# Release open webcam
+	# Release open input video
 	if not cap is None:
 		cap.release()
 
-	# Break loop if using input image
-	if not args.input_image is None:
+	# Break loop if using input image/video
+	if not args.input_image is None or not type(args.input_video) is int:
 		break
 
 	time.sleep(0.05)
